b7ba767613d45ac74f321f4ed35d661732976407
PH4R404H <42386290+PH4R404H@users.noreply.github.com>
2023-10-15 02:55:43 -0700

    Add files via upload

--- DragonPieceGPT/DragonPiece_TEST/DragonPiece.js
+++ DragonPieceGPT/DragonPiece_TEST/DragonPiece.js
@@ -56,3 +56,11 @@ class CustomSelect {
     new CustomSelect(selectElement);
   });
   
+  document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+    const body = document.body;
+
+    darkModeToggle.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+    });
+});
